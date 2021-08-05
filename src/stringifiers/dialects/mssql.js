e50cc2e9fdf1386088719d7ce921d275bc49e633
Andrew Palm <andrewtpalm@gmail.com>
2021-08-05 15:33:39 -0400

    Uncomment out compositeKey

--- src/stringifiers/dialects/mssql.js
+++ src/stringifiers/dialects/mssql.js
@@ -1,15 +1,16 @@
-"use strict";
+'use strict'
 
 function quote(str) {
   return `[${str}]`
 }
 
-module.exports = { ...require('./mixins/pagination-not-supported'),
+module.exports = {
+  ...require('./mixins/pagination-not-supported'),
   name: 'mssql',
   quote,
 
-  // compositeKey(parent, keys) {
-  //   keys = keys.map(key => `${quote(parent)}.${quote(key)}`);
-  //   return `CONCAT(${keys.join(', ')})`;
-  // }
-};
+  compositeKey(parent, keys) {
+    keys = keys.map(key => `${quote(parent)}.${quote(key)}`)
+    return `CONCAT(${keys.join(', ')})`
+  }
+}
